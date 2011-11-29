43ce1e26416effcc8ff1b16c21ade5a3fa08c0f0
tfotak <tfotak@80f082c3-aa3d-4a8a-bc4c-16289f4b763f>
2011-11-29 23:27:27 +0000

    #692 #773
    Changed URL mapping for default context to do a search
    
    Essentially does a default search from 1 to 15 (auto paging forward) for protocol
    OGC:WMS-1.1.1-http-get-map or OGC:WMS-1.3.0-http-get-map

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -7,7 +7,17 @@ class UrlMappings {
 			}
 		}
 
-		"/"(controller: "index", action: "queue")
+		"/" {
+			controller = "search"
+			action = "index"
+			from = '1'
+			// to parameter seems to be getting ignored at the moment
+			to = '15'
+			fast = 'false'
+			// Bit yucky but will do until I research a neat way of injecting the
+			// GrailsApplication into here
+			protocol = 'OGC:WMS-1.1.1-http-get-map or OGC:WMS-1.3.0-http-get-map'
+		}
 		"500"(view:'/error')
 	}
 }
