d0c9a251fcf2cfa99f0bd43bfe45f1c78ee599ca
tfotak <tfotak@80f082c3-aa3d-4a8a-bc4c-16289f4b763f>
2012-05-01 03:09:18 +0000

    Forgot to set the IMOS production config geonetwork endpoint
    Stupid Flanders

--- instances/ImosConfig.groovy
+++ instances/ImosConfig.groovy
@@ -12,8 +12,8 @@ environments {
 	
 	production {
 		grails.serverURL = "http://imossearch.emii.org.au"
-		geonetwork.search.serverURL = "http://mest.imos.org.au/geonetwork/srv/en/imos.xml.search"
-		geonetwork.index.serverURL = "http://mest.imos.org.au/geonetwork/srv/en/xml.search"
+		geonetwork.search.serverURL = "http://mest.imos.org.au/geonetwork/srv/eng/q"
+		geonetwork.index.serverURL = "http://mest.imos.org.au/geonetwork/srv/eng/q"
 		geoserver.response.cache.dir = "/tmp"
 		feature.missing.email.from='imossearch@emii.org.au'
 		feature.missing.email.subject='IMOS Search Index Action Required'
