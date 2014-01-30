734513026952731f3ed0bd8e1baf83000125c575
dnahodil <dnahodil@utas.edu.au>
2014-01-30 15:54:23 +1100

    Fixing problem with previous migration

--- grails-app/migrations/changelog.groovy
+++ grails-app/migrations/changelog.groovy
@@ -669,4 +669,24 @@ databaseChangeLog = {
             column(name: "constructor_args", value: "@gml:file_id,geom")
         }
     }
+
+    changeSet(author: "dnahodil", id: "1391053813000-1", failOnError: true) {
+
+        update(tableName: "feature_type_request_class") {
+            column(name: "constructor_args", valueComputed: "replace(constructor_args, '@gml:', '')")
+            where("""feature_type_name in (
+                'imos:anmn_burst_avg_timeseries_map',
+                'imos:abos_sofs_surfaceprop_rt_map',
+                'imos:aatams_sattag_nrt_map',
+                'imos:abos_sofs_surfaceprop_dm_map',
+                'imos:aatams_biologging_shearwater_map',
+                'imos:anmn_velocity_timeseries_map',
+                'imos:anmn_ts_timeseries_map',
+                'imos:aatams_biologging_penguin_map',
+                'imos:installation_summary',
+                'imos:faimms_timeseries_map',
+                'imos:soop_ba_trajectory_map')"""
+            )
+        }
+    }
 }
